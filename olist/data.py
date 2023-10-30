7e9741a971dbe94022229e9b7245ed0ca4f1489d
Matt Ieraci <vinniechaser@gmail.com>
2023-10-30 14:39:15 -0400

    Completed Olist class (get_data method

--- olist/data.py
+++ olist/data.py
@@ -9,13 +9,17 @@ class Olist:
         Its keys should be 'sellers', 'orders', 'order_items' etc...
         Its values should be pandas.DataFrames loaded from csv files
         """
-        # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
-            # Do not hardcode your path as it only works on your machine ('Users/username/code...')
-            # Use __file__ instead as an absolute path anchor independant of your usename
-            # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
-        # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
-        pass  # YOUR CODE HERE
-
+        csv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/csv')
+        
+        file_names = [file for file in os.listdir(csv_path) if file != ".keep"]
+        key_names = [name.replace(".csv", "").replace("_dataset", "").replace("olist_", "") for name in file_names]
+        
+        data = {}
+        for key, file_name in zip(key_names, file_names):
+            data[key] = pd.read_csv(os.path.join(csv_path, file_name))
+            
+        return data
+            
     def ping(self):
         """
         You call ping I print pong.
